85974230c381b602bd3e7dbcf0f69c5187e1d4af
Ana Rosa <asofiamcarvalho@gmail.com>
2023-02-03 21:41:18 +0100

    added user database and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,54 +9,77 @@ import Pokemon from "./components/Pokemon";
 import Header from "./components/Header";
 import User from "./components/User";
 import CreateNewUser from "./components/CreateNewUser";
-import PokeCart from './components/PokeCart';
-import PokemonInfo from './components/PokemonInfo';
+import PokeCart from "./components/PokeCart";
+import PokemonInfo from "./components/PokemonInfo";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [cart, setCart] = useState([]);
+  const [activeUser, setActiveUser] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/pokemon")
-      .then((res) => {
-        const response = res.data;
-        setPokemons(response);
-        console.log(response);
-        return response;
-      })
-      .then((response) => {
-        setSearchResults(response);
-      })
-      .catch((err) => {
+    const data = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/users/active");
+        setActiveUser(response.data);
+      } catch (err) {
         console.log(err);
-      });
+      }
+    };
+    data();
   }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:3001/pokemon")
+  //     .then((res) => {
+  //       const response = res.data;
+  //       setPokemons(response);
+  //       console.log(response);
+  //       return response;
+  //     })
+  //     .then((response) => {
+  //       setSearchResults(response);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   return (
     <div>
-
       {/* <div>
         <Search pokemons={pokemons} setSearchResults={setSearchResults} />
       </div>
       <div>
         <ListPage searchResults={searchResults} />
       </div> */}
-    
-      <Header />
-      
+
+      <Header activeUser={activeUser} setActiveUser={setActiveUser} />
+
       <Routes>
-      <Route path="/pokemon" element={<Pokemon pokemonId={Math.floor(Math.random()*1000)}/>} />
-        <Route path="/" element={<User />} />
-        <Route path="/CreateUser" element={<CreateNewUser />} />
-        <Route path="/pokemons" element={<PokemonInfo cart={cart} setCart={setCart}/>} />
-        <Route path="/cart" element={<PokeCart cart={cart} setCart={setCart} />} />
+        <Route
+          path="/pokemon"
+          element={<Pokemon pokemonId={Math.floor(Math.random() * 1000)} />}
+        />
+        <Route path="/" element={<User setActiveUser={setActiveUser} />} />
+        <Route
+          path="/CreateUser"
+          element={<CreateNewUser setActiveUser={setActiveUser} />}
+        />
+        <Route
+          path="/pokemons"
+          element={<PokemonInfo cart={cart} setCart={setCart} />}
+        />
+        <Route
+          path="/cart"
+          element={<PokeCart cart={cart} setCart={setCart} />}
+        />
       </Routes>
       {/* to be children of sindhuras component in new Route:"/" */}
       <Search pokemons={pokemons} setSearchResults={setSearchResults} />
       <ListPage searchResults={searchResults} />
-
     </div>
   );
 }
--- frontend/src/components/CreateNewUser.js
+++ frontend/src/components/CreateNewUser.js
@@ -1,28 +1,55 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Carousel from "react-bootstrap/Carousel";
 import InputGroup from "react-bootstrap/InputGroup";
-import ProfilePic1 from "../Images/ProfilePic1.png";
-import ProfilePic2 from "../Images/ProfilePic2.png";
-import ProfilePic3 from "../Images/ProfilePic3.png";
-import ProfilePic4 from "../Images/ProfilePic4.png";
-import ProfilePic5 from "../Images/ProfilePic5.png";
+import ProfilePic0 from "../Images/0.png";
+import ProfilePic1 from "../Images/1.png";
+import ProfilePic2 from "../Images/2.png";
+import ProfilePic3 from "../Images/3.png";
+import ProfilePic4 from "../Images/4.png";
 
-export default function CreateNewUser() {
+export default function CreateNewUser({ setActiveUser }) {
   const [picture, setPicture] = useState(0);
+  const [username, setUsername] = useState("");
 
   const handleSelect = (selectedIndex, e) => {
     setPicture(selectedIndex);
   };
-  // console.log(picture);
+
+  function handleChangeUsername(event) {
+    setUsername(event.target.value);
+  }
+  function handleClick(e) {
+    if (!username) {
+      e.preventDefault();
+      alert("please enter an username!");
+    }
+  }
+  const postData = {
+    img: picture,
+    username: username,
+  };
+
+  function createUser() {
+    if (username.length != 0) {
+      axios.post("http://localhost:3001/users", postData).then((res) => {
+        setActiveUser(res.data);
+      });
+    }
+  }
+
   return (
-    <div className="text-center mt-5 ">
-      <h2>Got it, let's sign you up!</h2>
-      <Card.Subtitle>Choose a Profile picture and username</Card.Subtitle>
-      <Card className="mx-auto my-5" style={{ width: "18rem" }}>
+    <div className="text-center mt-5  ">
+      <h2 className="GameBoyFont">Got it, let's sign you up!</h2>
+      <Card.Subtitle className="GameBoyFont">
+        Choose a Profile picture and username
+      </Card.Subtitle>
+      <Card className="CarouselContainer">
         <Carousel
           variant="dark"
           style={{ backgroundColor: "none" }}
@@ -31,50 +58,39 @@ export default function CreateNewUser() {
           indicators={false}
         >
           <Carousel.Item interval={1000000}>
-            <img
-              src={ProfilePic1}
-              className="img-fluid max-height: 100% width: auto"
-            />
+            <img src={ProfilePic0} className="profilePicBig" />
           </Carousel.Item>
           <Carousel.Item interval={1000000}>
-            <img
-              src={ProfilePic2}
-              className="img-fluid max-width: 100% height: auto"
-            />
+            <img src={ProfilePic1} className="profilePicBig" />
           </Carousel.Item>
           <Carousel.Item interval={1000000}>
-            <img
-              src={ProfilePic3}
-              className="img-fluid max-width: 100% height: auto"
-            />
+            <img src={ProfilePic2} className="profilePicBig" />
           </Carousel.Item>
           <Carousel.Item interval={1000000}>
-            <img
-              src={ProfilePic4}
-              className="img-fluid max-width: 100% height: auto"
-            />
+            <img src={ProfilePic3} className="profilePicBig" />
           </Carousel.Item>
           <Carousel.Item interval={1000000}>
-            <img
-              src={ProfilePic5}
-              className="img-fluid max-width: 100% height: auto"
-            />
+            <img src={ProfilePic4} className="profilePicBig" />
           </Carousel.Item>
         </Carousel>
         <Card.Body></Card.Body>
       </Card>
       <InputGroup className="mb-5 w-25 mx-auto">
-        <InputGroup.Text id="inputGroup-sizing-default">
+        <InputGroup.Text id="inputGroup-sizing-sm" className="inputUsername">
           Username:
         </InputGroup.Text>
         <Form.Control
           aria-label="Username"
-          aria-describedby="inputGroup-sizing-default"
+          aria-describedby="inputGroup-sizing-sm"
+          className="GameBoyFont"
+          onChange={handleChangeUsername}
         />
       </InputGroup>
-      <Button variant="primary" className="mb-5">
-        Submit
-      </Button>
+      <NavLink to="/pokemons" onClick={handleClick}>
+        <Button variant="primary" onClick={createUser} disabled={!username}>
+          Sign up
+        </Button>
+      </NavLink>
     </div>
   );
 }
